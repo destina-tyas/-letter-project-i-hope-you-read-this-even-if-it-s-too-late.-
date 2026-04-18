d24d816efbcb09c28852818ab70bcaa2f67b37a9
destina-tyas <destinatyas9@gmail.com>
2026-04-18 20:06:11 +0700

    Update birthday message in script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const message = `I don’t even know if you’ll ever read this.\nBut if you do… I just want you to know, I still think about you sometimes.\n\nWe both tried. We both cared. But the universe just never gave us enough space to become “us.”\n\nI just wonder… if we had met at a different point in life, would the ending have been different?\n\nAnyway, I hope you're doing okay, even though we’re not in each other’s lives anymore.`;
+const message = `I don’t even know if you’ll ever read this.\nBut if you do… I just want you to know, I still think about you sometimes.\n\nbtw happy birthday ya ell,niat loh aku gae website wkwk.btw tolak aku secara langsung dong ell,kalau begini soalnya aku jadi on off cegilnya wkwk.happy birthday,all the best on your special day!many happy returns of the day\n\n\n\n.`;
 
 function showLetter() {
   document.getElementById("introText").style.opacity = 0;
