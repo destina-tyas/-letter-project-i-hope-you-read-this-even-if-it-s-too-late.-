274840be869a9e0a09b98a12df4c42aa61b7cc9b
destina-tyas <destinatyas9@gmail.com>
2026-04-18 20:19:24 +0700

    Fix message formatting in script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const message = `I don’t even know if you’ll ever read this.\nBut if you do… I just want you to know, I still think about you sometimes.\n\nbtw happy birthday ya ell,niat loh aku gae website wkwk.btw tolak aku secara langsung dong ell,kalau begini soalnya aku jadi on off cegilnya wkwk.happy birthday,all the best on your special day!many happy returns of the day\n\n\n\n.`;
+const message = `I don’t even know if you’ll ever read this.\nBut if you do… I just want you to know, I still think about you sometimes.\n\nbtw happy birthday ya ell,niat loh aku gae website wkwk.btw tolak aku secara langsung dong ell,kalau begini soalnya aku jadi on off cegilnya wkwk.happy birthday,all the best on your special day!many happy returns of the day.`;
 
 function showLetter() {
   document.getElementById("introText").style.opacity = 0;
